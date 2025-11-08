d08fefc19fba2482c5e2723cab281f917214e055
Alikhan Oitan <alikhan.oitan@gmail.com>
2025-11-09 04:54:49 +0500

    remove controls and change text to prayers. Closes #2

--- cmd/salah-tui/main.go
+++ cmd/salah-tui/main.go
@@ -5,7 +5,7 @@ import (
 	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
+	gloss "github.com/charmbracelet/lipgloss"
 )
 
 type model struct {
@@ -18,33 +18,27 @@ func (m model) Init() tea.Cmd {
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
-
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "q", "ctrl+c":
 			return m, tea.Quit
-		case "up":
-			m.count++
-		case "down":
-			m.count--
 		}
 	}
 	return m, nil
 }
 
 func (m model) View() string {
-	style := lipgloss.NewStyle().
+	style := gloss.NewStyle().
 		Bold(true).
-		Foreground(lipgloss.Color("#04B575"))
+		Foreground(gloss.Color("#04B575"))
 
-	return fmt.Sprintf("%s\n\n↑/↓ to change, q to quit", style.Render(fmt.Sprintf("Count: %d", m.count)))
+	return fmt.Sprintf("%s\n\nq to quit", style.Render(fmt.Sprintf("Total prayers: %d", m.count)))
 }
 
 func main() {
 	p := tea.NewProgram(model{})
-	if err := p.Start(); err != nil {
+	if _, err := p.Run(); err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
 }
-
