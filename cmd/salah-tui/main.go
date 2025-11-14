bf60cb56592c390fef69177eab3984568a058a58
Alikhan Oitan <alikhan.oitan@gmail.com>
2025-11-15 02:18:41 +0500

    add a second tab for todays date

--- cmd/salah-tui/main.go
+++ cmd/salah-tui/main.go
@@ -3,13 +3,32 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
 	gloss "github.com/charmbracelet/lipgloss"
 )
 
+const TAB_COUNT = 2
+
 type model struct {
 	count int
+	tab   int
+}
+
+func initialModel() model { return model{tab: 0} }
+
+func (m *model) switchTab() {
+	m.tab = (m.tab + 1) % TAB_COUNT
+}
+
+func (m *model) showStatsView(style gloss.Style) string {
+	return style.Render(fmt.Sprintf("Total prayers: %d", m.count))
+}
+
+func (m *model) showEntryView(style gloss.Style) string {
+	today := time.Now().Format("2006-01-02")
+	return style.Render(fmt.Sprintf("%s: Fajr, Dhuhr, Asr, Maghrib, Isha", today))
 }
 
 func (m model) Init() tea.Cmd {
@@ -19,6 +38,10 @@ func (m model) Init() tea.Cmd {
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyTab:
+			m.switchTab()
+		}
 		switch msg.String() {
 		case "q", "ctrl+c":
 			return m, tea.Quit
@@ -32,11 +55,13 @@ func (m model) View() string {
 		Bold(true).
 		Foreground(gloss.Color("#04B575"))
 
-	return fmt.Sprintf("%s\n\nq to quit", style.Render(fmt.Sprintf("Total prayers: %d", m.count)))
+	views := []string{m.showStatsView(style), m.showEntryView(style)}
+
+	return fmt.Sprintf("%s\n\nq to quit", views[m.tab])
 }
 
 func main() {
-	p := tea.NewProgram(model{})
+	p := tea.NewProgram(initialModel())
 	if _, err := p.Run(); err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
